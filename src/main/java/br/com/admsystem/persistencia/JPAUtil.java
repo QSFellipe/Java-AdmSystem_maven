b8e03de99605b02287a1f5f7290ff7952081b41c
Fellipe Queiroz Silva <qsfellipe@gmail.com>
2025-02-27 18:33:00 -0300

    Classes de persistência
    Criação das classes de persistência:
    - CadastroTransacoesDAO;
    - RelatorioDAO;
    - UsuarioDAO.

---
+++ src/main/java/br/com/admsystem/persistencia/JPAUtil.java
@@ -0,0 +1,36 @@
+package br.com.admsystem.persistencia;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JPAUtil {
+
+    private static final String PERSISTENCE_UNIT = "adm_system";
+
+    private static EntityManager em;
+    private static EntityManagerFactory emf;
+
+    public static EntityManager getEntityManager() {
+        
+        //caso o EntityManagerFactory esteja nulo ou não esteja aberto, cria-se o EntityManagerFactory
+        if (emf == null || !emf.isOpen()) {
+            emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+        }
+        
+        //caso o EntityManager esteja nulo ou não esteja aberto, cria-se o EntityManager
+        if (em == null || !em.isOpen()) {
+            em = emf.createEntityManager();
+        }
+
+        return em;
+    }
+
+    public static void closeEntityManager() {
+        //caso o EntityManagerFactory esteja aberto fecha-se o em e o emf
+        if (em.isOpen() && em != null) {
+            em.close();
+            emf.close();
+        }
+    }
+}
---
+++ src/main/java/br/com/admsystem/persistencia/RelatarioDAO.java
@@ -0,0 +1,6 @@
+
+package br.com.admsystem.persistencia;
+
+public class RelatarioDAO {
+    
+}
